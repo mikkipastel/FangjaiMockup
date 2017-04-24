2e0b582f3ab7da954116f69e295af78cd82c72cc
mikkipastel <acaideddesign3@gmail.com>
2017-04-24 22:42:16 +0700

    finish for implement ^_^

--- app/src/main/java/com/mikkipastel/fangjaimockup/View/AdsListItem.java
+++ app/src/main/java/com/mikkipastel/fangjaimockup/View/AdsListItem.java
@@ -22,13 +22,10 @@ public class AdsListItem {
     public void setImageAds(Context context, String picurl) {
         Glide.with(context)
                 .load(picurl)
-                .asBitmap()
                 .error(R.drawable.fungjai_logo_white)
                 .placeholder(R.drawable.fungjai_logo_white)
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)
                 .into(imageAds);
-
-
     }
 
 }

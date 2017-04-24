cea972d9f41d3c8644041b14158e430a1d3e7176
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2017-04-24 10:30:21 +0700

    Update AdsListItem.java

--- app/src/main/java/com/mikkipastel/fangjaimockup/View/AdsListItem.java
+++ app/src/main/java/com/mikkipastel/fangjaimockup/View/AdsListItem.java
@@ -3,6 +3,7 @@ package com.mikkipastel.fangjaimockup.View;
 import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -15,9 +16,11 @@ import com.mikkipastel.fangjaimockup.R;
 
 public class AdsListItem {
     ImageView imageAds;
+    TextView nameAds;
 
     public AdsListItem(View parent) {
         imageAds = (ImageView) parent.findViewById(R.id.imageAds);
+        nameAds = (TextView) parent.findViewById(R.id.adsName);
     }
 
     //set ads cover in app
@@ -28,6 +31,11 @@ public class AdsListItem {
                 .placeholder(R.drawable.fungjai_logo_white)
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)
                 .into(imageAds);
+    }
+    
+    //add video name because give information abou this to user
+    public void setNameAds(String name){
+        nameAds.setText(name);
     }
     
     //add to scale cover to 4:3
